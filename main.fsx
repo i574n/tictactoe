f738ef5120ca1653019836403a3a406f711ad0df
fc1943s <root@fc1943s.dev>
2022-05-04 23:10:33 -0300

    console

--- main.fsx
+++ main.fsx
@@ -2,24 +2,35 @@ let rec method0 () : int32 =
     180
 and closure0 () () : int32 =
     method0()
-and method1 (v0 : string) : unit =
-    printfn $"%A{v0}"
-and method2 (v0 : bool) : unit =
-    let v1 : bool = v0 = false
-    if v1 then
-        failwith<unit> "test"
-and method3 (v0 : (int32 [])) : unit =
-    printfn $"%A{v0}"
+and closure1 () (v0 : string) : unit =
+    AnsiConsole.WriteLine v0
+and method2 () : (string -> unit) =
+    closure1()
+and method1 () : unit =
+    AnsiConsole.WriteLine ()
+    let v0 : (int32 -> Color) = Color.FromInt32
+    let v1 : Color = v0 5
+    let v2 : Style = Style v1
+    let v3 : Rule = Rule "[white bold underline]Title 1[/]"
+    let v4 : Rule = v3.LeftAligned ()
+    let v5 : Rule = v4.RuleStyle v2
+    AnsiConsole.Write v5
+    let v6 : string = System.String ('w', 17)
+    printfn $"%A{v6}"
+    let v7 : (int32 []) = [|5|]
+    printfn $"%A{v7}"
+    let v8 : (string -> unit) = method2()
+    v8 "end"
+    let v9 : (int32 -> Color) = Color.FromInt32
+    let v10 : Color = v9 5
+    let v11 : Style = Style v10
+    let v12 : Rule = Rule ()
+    let v13 : Rule = v12.LeftAligned ()
+    let v14 : Rule = v13.RuleStyle v11
+    AnsiConsole.Write v14
 let v0 : (unit -> int32) = closure0()
 let v1 : string = $"main.spi Loaded. Width: {v0 ()}"
-method1(v1)
-let v2 : string = "/########################################\\ start"
-method1(v2)
-let v3 : bool = true
-method2(v3)
-let v4 : string = System.String ('q', 17)
-method1(v4)
-let v5 : (int32 []) = [|3|]
-method3(v5)
-let v6 : string = "\\########################################/ end"
-method1(v6)
+printfn $"%A{v1}"
+#r "nuget: Spectre.Console"
+open Spectre.Console
+method1()
--- main.pyx
+++ main.pyx
@@ -4,43 +4,53 @@ cdef class Closure0():
     def __init__(self): pass
     def __call__(self):
         return method0()
+cdef class Closure1():
+    def __init__(self): pass
+    def __call__(self, str v0):
+        AnsiConsole.WriteLine v0
 cdef signed long method0():
     return (<signed long>180)
-cdef void method1(str v0):
-    print(v0)
-cdef void method2(bint v0):
-    cdef bint v1
-    v1 = v0 == 0
-    if v1:
-        raise Exception("test")
-    else:
-        pass
-cdef void method3(numpy.ndarray[signed long,ndim=1] v0):
-    print(v0)
+cdef object method2():
+    return Closure1()
+cdef void method1():
+    cdef object v0
+    cdef Color v1
+    cdef Style v2
+    cdef Rule v3
+    cdef Rule v4
+    cdef Rule v5
+    cdef str v6
+    cdef numpy.ndarray[signed long,ndim=1] v7
+    cdef object v8
+    v0 = Color.FromInt32
+    v1 = v0((<signed long>5))
+    del v0
+    v2 = Style v1
+    del v1
+    v3 = Rule "[white bold underline]Title 1[/]"
+    v4 = v3.LeftAligned ()
+    del v3
+    v5 = v4.RuleStyle v2
+    del v2; del v4
+    AnsiConsole.Write v5
+    del v5
+    v6 = System.String ('w', (<signed long>17))
+    printfn $"%A{v6}"
+    del v6
+    v7 = numpy.empty(1,dtype=numpy.int32)
+    v7[0] = (<signed long>5)
+    printfn $"%A{v7}"
+    del v7
+    v8 = method2()
+    v8("end")
 cpdef void main():
     cdef object v0
     cdef str v1
-    cdef str v2
-    cdef bint v3
-    cdef str v4
-    cdef numpy.ndarray[signed long,ndim=1] v5
-    cdef str v6
     v0 = Closure0()
     v1 = $"main.spi Loaded. Width: {v0 ()}"
     del v0
-    method1(v1)
+    printfn $"%A{v1}"
     del v1
-    v2 = "/########################################\\ start"
-    method1(v2)
-    del v2
-    v3 = 1
-    method2(v3)
-    v4 = str???('q', (<signed long>17))
-    method1(v4)
-    del v4
-    v5 = numpy.empty(1,dtype=numpy.int32)
-    v5[0] = (<signed long>3)
-    method3(v5)
-    del v5
-    v6 = "\\########################################/ end"
-    method1(v6)
+    #r "nuget: Spectre.Console"
+    open Spectre.Console
+    method1()
