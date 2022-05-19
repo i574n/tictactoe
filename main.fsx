d943f61a4d50c2939c101c87c7e07e1622b08858
fc1943s <root@fc1943s.dev>
2022-05-19 04:51:48 +0000

    money_refund_logic

--- main.fsx
+++ main.fsx
@@ -1,3 +1,6 @@
+#r "nuget: Fable.Python"
+open Fable.Core.PyInterop
+open Fable.Core
 let rec method0 () : struct (string * string * string * string * string * string * string * string * string) =
     let v0 : string = (importAll "pyteal")?Bytes "PlayerXState"
     let v1 : string = (importAll "pyteal")?Bytes "PlayerOState"
@@ -9,7 +12,7 @@ let rec method0 () : struct (string * string * string * string * string * string
     let v7 : string = (importAll "pyteal")?Bytes "GameState"
     let v8 : string = (importAll "pyteal")?Bytes "BetAmount"
     struct (v6, v8, v5, v7, v3, v1, v4, v2, v0)
-and closure0 () () : obj =
+and closure0 () () : bool =
     let v0 : string = "app_initialization_logic"
     printfn $"%A{v0}"
     let v1 : int32 = (importAll "pyteal")?Int 0
@@ -18,13 +21,13 @@ and closure0 () () : obj =
     let v4 : int32 = (importAll "pyteal")?Int 1000000
     let v5 : int32 = (importAll "pyteal")?Int 3600
     let struct (v6 : string, v7 : string, v8 : string, v9 : string, v10 : string, v11 : string, v12 : string, v13 : string, v14 : string) = method0()
-    let v15 : obj = (importAll "pyteal")?App?globalPut (v14, v1)
-    let v16 : obj = (importAll "pyteal")?App?globalPut (v11, v2)
-    let v17 : obj = (importAll "pyteal")?App?globalPut (v9, v3)
-    let v18 : obj = (importAll "pyteal")?App?globalPut (v7, v4)
+    let v15 : bool = (importAll "pyteal")?App?globalPut (v14, v1)
+    let v16 : bool = (importAll "pyteal")?App?globalPut (v11, v2)
+    let v17 : bool = (importAll "pyteal")?App?globalPut (v9, v3)
+    let v18 : bool = (importAll "pyteal")?App?globalPut (v7, v4)
     let v19 : int32 = (importAll "pyteal")?Int 1
-    let v20 : obj = (importAll "pyteal")?Return v19
-    let v21 : (obj []) = [|v15; v16; v17; v18; v20|]
+    let v20 : bool = (importAll "pyteal")?Return v19
+    let v21 : (bool []) = [|v15; v16; v17; v18; v20|]
     (importAll "pyteal")?Seq v21
 and closure1 () () : bool =
     let v0 : string = "is_tie"
@@ -35,7 +38,7 @@ and closure1 () () : bool =
     let v12 : int32 = (importAll "pyteal")?Int 511
     let v13 : int32 = (importAll "pyteal")?BitwiseOr v10 v11
     v12 = v13
-and closure2 () (v0 : int32) : obj =
+and closure2 () (v0 : int32) : bool =
     let v1 : string = "has_player_won"
     printfn $"%A{v1}"
     let v2 : int32 = (importAll "pyteal")?Int 448
@@ -70,16 +73,131 @@ and closure2 () (v0 : int32) : obj =
     let v31 : int32 = (importAll "pyteal")?BitwiseAnd v0 v30
     let v32 : int32 = (importAll "pyteal")?Int 84
     let v33 : bool = v31 = v32
-    let v34 : obj = (importAll "pyteal")?Or v5 v9 v13 v17 v21 v25 v29 v33
+    let v34 : bool = (importAll "pyteal")?Or v5 v9 v13 v17 v21 v25 v29 v33
     let v35 : int32 = (importAll "pyteal")?Int 1
     let v36 : int32 = (importAll "pyteal")?Int 0
     (importAll "pyteal")?If v34 v35 v36
-#r "nuget: Fable.Python"
-open Fable.Core.PyInterop
-open Fable.Core
+and closure3 () () : bool =
+    let v0 : string = "money_refund_logic"
+    printfn $"%A{v0}"
+    let struct (v1 : string, v2 : string, v3 : string, v4 : string, v5 : string, v6 : string, v7 : string, v8 : string, v9 : string) = method0()
+    let v10 : int32 = (importAll "pyteal")?App?globalGet v4
+    let v11 : int32 = (importAll "pyteal")?Int 1
+    let v12 : bool = v10 = v11
+    let v13 : int32 = (importAll "pyteal")?App?globalGet v4
+    let v14 : int32 = (importAll "pyteal")?Int 0
+    let v15 : bool = v13 = v14
+    let v16 : int32 = (importAll "pyteal")?Global?latest_timestamp ()
+    let v17 : int32 = (importAll "pyteal")?App?globalGet v1
+    let v18 : bool = v16 > v17
+    let v19 : int32 = (importAll "pyteal")?App?globalGet v7
+    let v20 : int32 = (importAll "pyteal")?App?globalGet v5
+    let v21 : bool = v19 = v20
+    let v22 : bool = (importAll "pyteal")?And v15 v18 v21
+    let v23 : bool = (importAll "pyteal")?Or v12 v22
+    let v24 : int32 = (importAll "pyteal")?App?globalGet v4
+    let v25 : int32 = (importAll "pyteal")?Int 2
+    let v26 : bool = v24 = v25
+    let v27 : int32 = (importAll "pyteal")?App?globalGet v4
+    let v28 : int32 = (importAll "pyteal")?Int 0
+    let v29 : bool = v27 = v28
+    let v30 : int32 = (importAll "pyteal")?Global?latest_timestamp ()
+    let v31 : int32 = (importAll "pyteal")?App?globalGet v1
+    let v32 : bool = v30 > v31
+    let v33 : int32 = (importAll "pyteal")?App?globalGet v7
+    let v34 : int32 = (importAll "pyteal")?App?globalGet v8
+    let v35 : bool = v33 = v34
+    let v36 : bool = (importAll "pyteal")?And v29 v32 v35
+    let v37 : bool = (importAll "pyteal")?Or v26 v36
+    let v38 : obj = (import "TxnGroup" "pyteal")?__getitem__(import "Gtxn" "pyteal", 1)
+    let v39 : int32 = v38?receiver ()
+    let v40 : int32 = (importAll "pyteal")?App?globalGet v8
+    let v41 : bool = v39 = v40
+    let v42 : bool = (importAll "pyteal")?Assert v41
+    let v43 : obj = (import "TxnGroup" "pyteal")?__getitem__(import "Gtxn" "pyteal", 1)
+    let v44 : int32 = v43?amount ()
+    let v45 : int32 = (importAll "pyteal")?Int 2
+    let v46 : int32 = (importAll "pyteal")?App?globalGet v2
+    let v47 : int32 = v45 * v46
+    let v48 : bool = v44 = v47
+    let v49 : bool = (importAll "pyteal")?Assert v48
+    let v50 : int32 = (importAll "pyteal")?Int 1
+    let v51 : bool = (importAll "pyteal")?App?globalPut (v4, v50)
+    let v52 : (bool []) = [|v42; v49; v51|]
+    let v53 : bool = (importAll "pyteal")?Seq v52
+    let v54 : obj = (import "TxnGroup" "pyteal")?__getitem__(import "Gtxn" "pyteal", 1)
+    let v55 : int32 = v54?receiver ()
+    let v56 : int32 = (importAll "pyteal")?App?globalGet v5
+    let v57 : bool = v55 = v56
+    let v58 : bool = (importAll "pyteal")?Assert v57
+    let v59 : obj = (import "TxnGroup" "pyteal")?__getitem__(import "Gtxn" "pyteal", 1)
+    let v60 : int32 = v59?amount ()
+    let v61 : int32 = (importAll "pyteal")?Int 2
+    let v62 : int32 = (importAll "pyteal")?App?globalGet v2
+    let v63 : int32 = v61 * v62
+    let v64 : bool = v60 = v63
+    let v65 : bool = (importAll "pyteal")?Assert v64
+    let v66 : int32 = (importAll "pyteal")?Int 2
+    let v67 : bool = (importAll "pyteal")?App?globalPut (v4, v66)
+    let v68 : (bool []) = [|v58; v65; v67|]
+    let v69 : bool = (importAll "pyteal")?Seq v68
+    let v70 : int32 = (importAll "pyteal")?App?globalGet v4
+    let v71 : int32 = (importAll "pyteal")?Int 3
+    let v72 : bool = v70 = v71
+    let v73 : obj = (import "TxnGroup" "pyteal")?__getitem__(import "Gtxn" "pyteal", 1)
+    let v74 : int32 = v73?receiver ()
+    let v75 : int32 = (importAll "pyteal")?App?globalGet v8
+    let v76 : bool = v74 = v75
+    let v77 : bool = (importAll "pyteal")?Assert v76
+    let v78 : obj = (import "TxnGroup" "pyteal")?__getitem__(import "Gtxn" "pyteal", 1)
+    let v79 : int32 = v78?amount ()
+    let v80 : int32 = (importAll "pyteal")?App?globalGet v2
+    let v81 : bool = v79 = v80
+    let v82 : bool = (importAll "pyteal")?Assert v81
+    let v83 : obj = (import "TxnGroup" "pyteal")?__getitem__(import "Gtxn" "pyteal", 2)
+    let v84 : int32 = v83?type_enum ()
+    let v85 : int32 = (importAll "pyteal")?TxnType?Payment
+    let v86 : bool = v84 = v85
+    let v87 : bool = (importAll "pyteal")?Assert v86
+    let v88 : obj = (import "TxnGroup" "pyteal")?__getitem__(import "Gtxn" "pyteal", 2)
+    let v89 : int32 = v88?sender ()
+    let v90 : int32 = (importAll "pyteal")?App?globalGet v3
+    let v91 : bool = v89 = v90
+    let v92 : bool = (importAll "pyteal")?Assert v91
+    let v93 : obj = (import "TxnGroup" "pyteal")?__getitem__(import "Gtxn" "pyteal", 2)
+    let v94 : int32 = v93?receiver ()
+    let v95 : int32 = (importAll "pyteal")?App?globalGet v5
+    let v96 : bool = v94 = v95
+    let v97 : bool = (importAll "pyteal")?Assert v96
+    let v98 : obj = (import "TxnGroup" "pyteal")?__getitem__(import "Gtxn" "pyteal", 2)
+    let v99 : int32 = v98?amount ()
+    let v100 : int32 = (importAll "pyteal")?App?globalGet v2
+    let v101 : bool = v99 = v100
+    let v102 : bool = (importAll "pyteal")?Assert v101
+    let v103 : (bool []) = [|v77; v82; v87; v92; v97; v102|]
+    let v104 : bool = (importAll "pyteal")?Seq v103
+    let v105 : obj = (import "TxnGroup" "pyteal")?__getitem__(import "Gtxn" "pyteal", 1)
+    let v106 : int32 = v105?type_enum ()
+    let v107 : int32 = (importAll "pyteal")?TxnType?Payment
+    let v108 : bool = v106 = v107
+    let v109 : bool = (importAll "pyteal")?Assert v108
+    let v110 : obj = (import "TxnGroup" "pyteal")?__getitem__(import "Gtxn" "pyteal", 1)
+    let v111 : int32 = v110?sender ()
+    let v112 : int32 = (importAll "pyteal")?App?globalGet v3
+    let v113 : bool = v111 = v112
+    let v114 : bool = (importAll "pyteal")?Assert v113
+    let v115 : (bool []) = [|v23; v53|]
+    let v116 : (bool []) = [|v37; v69|]
+    let v117 : (bool []) = [|v72; v104|]
+    let v118 : bool = (importAll "pyteal")?Cond v115 v116 v117
+    let v119 : int32 = (importAll "pyteal")?Int 1
+    let v120 : bool = (importAll "pyteal")?Return v119
+    let v121 : (bool []) = [|v109; v114; v118; v120|]
+    (importAll "pyteal")?Seq v121
 let v0 : string = "main.spi Loaded"
 printfn $"%A{v0}"
-let v1 : (unit -> obj) = closure0()
+let v1 : (unit -> bool) = closure0()
 let v2 : (unit -> bool) = closure1()
-let v3 : (int32 -> obj) = closure2()
+let v3 : (int32 -> bool) = closure2()
+let v4 : (unit -> bool) = closure3()
 ()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pyteal
-from typing import (Tuple, Any, List, Callable)
+from pyteal import (TxnGroup, Gtxn)
+from typing import (Tuple, List, Any, Callable)
 from fable_modules.fable_library.string import (to_console, interpolate)
 
 def method0() -> Tuple[str, str, str, str, str, str, str, str, str]:
@@ -7,27 +8,27 @@ def method0() -> Tuple[str, str, str, str, str, str, str, str, str]:
     v1_1 : str = pyteal.Bytes("PlayerOState")
     v2_1 : str = pyteal.Bytes("PlayerXAddress")
     v3_1 : str = pyteal.Bytes("PlayerOAddress")
-    v4 : str = pyteal.Bytes("PlayerTurnAddress")
+    v4_1 : str = pyteal.Bytes("PlayerTurnAddress")
     v5 : str = pyteal.Bytes("FundsEscrowAddress")
     v6 : str = pyteal.Bytes("ActionTimeout")
     v7 : str = pyteal.Bytes("GameState")
-    return (v6, pyteal.Bytes("BetAmount"), v5, v7, v3_1, v1_1, v4, v2_1, v0_1)
+    return (v6, pyteal.Bytes("BetAmount"), v5, v7, v3_1, v1_1, v4_1, v2_1, v0_1)
 
 
-def closure0(unit_var0: None, unit_var1: None) -> Any:
+def closure0(unit_var0: None, unit_var1: None) -> bool:
     to_console(interpolate("%A%P()", ["app_initialization_logic"]))
     v1_1 : int = (pyteal.Int(0)) or 0
     v2_1 : int = (pyteal.Int(0)) or 0
     v3_1 : int = (pyteal.Int(0)) or 0
-    v4 : int = (pyteal.Int(1000000)) or 0
+    v4_1 : int = (pyteal.Int(1000000)) or 0
     v5 : int = (pyteal.Int(3600)) or 0
     pattern_input : Tuple[str, str, str, str, str, str, str, str, str] = method0()
-    v15 : Any = pyteal.App.globalPut(pattern_input[8], v1_1)
-    v16 : Any = pyteal.App.globalPut(pattern_input[5], v2_1)
-    v17 : Any = pyteal.App.globalPut(pattern_input[3], v3_1)
-    v18 : Any = pyteal.App.globalPut(pattern_input[1], v4)
+    v15 : bool = pyteal.App.globalPut(pattern_input[8], v1_1)
+    v16 : bool = pyteal.App.globalPut(pattern_input[5], v2_1)
+    v17 : bool = pyteal.App.globalPut(pattern_input[3], v3_1)
+    v18 : bool = pyteal.App.globalPut(pattern_input[1], v4_1)
     v19 : int = (pyteal.Int(1)) or 0
-    v21 : List[Any] = [v15, v16, v17, v18, pyteal.Return(v19)]
+    v21 : List[bool] = [v15, v16, v17, v18, pyteal.Return(v19)]
     return pyteal.Seq(v21)
 
 
@@ -39,7 +40,7 @@ def closure1(unit_var0: None, unit_var1: None) -> bool:
     return (pyteal.Int(511)) == (pyteal.BitwiseOr(v10, v11))
 
 
-def closure2(unit_var0: None, v0_1: int) -> Any:
+def closure2(unit_var0: None, v0_1: int) -> bool:
     to_console(interpolate("%A%P()", ["has_player_won"]))
     v2_1 : int = (pyteal.Int(448)) or 0
     v5 : bool = (pyteal.BitwiseAnd(v0_1, v2_1)) == (pyteal.Int(448))
@@ -57,21 +58,96 @@ def closure2(unit_var0: None, v0_1: int) -> Any:
     v29 : bool = (pyteal.BitwiseAnd(v0_1, v26)) == (pyteal.Int(273))
     v30 : int = (pyteal.Int(84)) or 0
     v33 : bool = (pyteal.BitwiseAnd(v0_1, v30)) == (pyteal.Int(84))
-    v34 : Any = pyteal.Or(v5, v9, v13, v17, v21, v25, v29, v33)
+    v34 : bool = pyteal.Or(v5, v9, v13, v17, v21, v25, v29, v33)
     v35 : int = (pyteal.Int(1)) or 0
     v36 : int = (pyteal.Int(0)) or 0
     return pyteal.If(v34, v35, v36)
+
+
+def closure3(unit_var0: None, unit_var1: None) -> bool:
+    to_console(interpolate("%A%P()", ["money_refund_logic"]))
+    pattern_input : Tuple[str, str, str, str, str, str, str, str, str] = method0()
+    v8 : str = pattern_input[7]
+    v7 : str = pattern_input[6]
+    v5 : str = pattern_input[4]
+    v4_1 : str = pattern_input[3]
+    v3_1 : str = pattern_input[2]
+    v2_1 : str = pattern_input[1]
+    v1_1 : str = pattern_input[0]
+    v12 : bool = (pyteal.App.globalGet(v4_1)) == (pyteal.Int(1))
+    v15 : bool = (pyteal.App.globalGet(v4_1)) == (pyteal.Int(0))
+    v18 : bool = (pyteal.Global.latest_timestamp()) > (pyteal.App.globalGet(v1_1))
+    v21 : bool = (pyteal.App.globalGet(v7)) == (pyteal.App.globalGet(v5))
+    v22 : bool = pyteal.And(v15, v18, v21)
+    v23 : bool = pyteal.Or(v12, v22)
+    v26 : bool = (pyteal.App.globalGet(v4_1)) == (pyteal.Int(2))
+    v29 : bool = (pyteal.App.globalGet(v4_1)) == (pyteal.Int(0))
+    v32 : bool = (pyteal.Global.latest_timestamp()) > (pyteal.App.globalGet(v1_1))
+    v35 : bool = (pyteal.App.globalGet(v7)) == (pyteal.App.globalGet(v8))
+    v36 : bool = pyteal.And(v29, v32, v35)
+    v37 : bool = pyteal.Or(v26, v36)
+    v38 : Any = TxnGroup.__getitem__(Gtxn, 1)
+    v41 : bool = (v38.receiver()) == (pyteal.App.globalGet(v8))
+    v42 : bool = pyteal.Assert(v41)
+    v43 : Any = TxnGroup.__getitem__(Gtxn, 1)
+    v48 : bool = (v43.amount()) == ((pyteal.Int(2)) * (pyteal.App.globalGet(v2_1)))
+    v49 : bool = pyteal.Assert(v48)
+    v50 : int = (pyteal.Int(1)) or 0
+    v52 : List[bool] = [v42, v49, pyteal.App.globalPut(v4_1, v50)]
+    v53 : bool = pyteal.Seq(v52)
+    v54 : Any = TxnGroup.__getitem__(Gtxn, 1)
+    v57 : bool = (v54.receiver()) == (pyteal.App.globalGet(v5))
+    v58 : bool = pyteal.Assert(v57)
+    v59 : Any = TxnGroup.__getitem__(Gtxn, 1)
+    v64 : bool = (v59.amount()) == ((pyteal.Int(2)) * (pyteal.App.globalGet(v2_1)))
+    v65 : bool = pyteal.Assert(v64)
+    v66 : int = (pyteal.Int(2)) or 0
+    v68 : List[bool] = [v58, v65, pyteal.App.globalPut(v4_1, v66)]
+    v69 : bool = pyteal.Seq(v68)
+    v72 : bool = (pyteal.App.globalGet(v4_1)) == (pyteal.Int(3))
+    v73 : Any = TxnGroup.__getitem__(Gtxn, 1)
+    v76 : bool = (v73.receiver()) == (pyteal.App.globalGet(v8))
+    v77 : bool = pyteal.Assert(v76)
+    v78 : Any = TxnGroup.__getitem__(Gtxn, 1)
+    v81 : bool = (v78.amount()) == (pyteal.App.globalGet(v2_1))
+    v82 : bool = pyteal.Assert(v81)
+    v83 : Any = TxnGroup.__getitem__(Gtxn, 2)
+    v86 : bool = (v83.type_enum()) == pyteal.TxnType.Payment
+    v87 : bool = pyteal.Assert(v86)
+    v88 : Any = TxnGroup.__getitem__(Gtxn, 2)
+    v91 : bool = (v88.sender()) == (pyteal.App.globalGet(v3_1))
+    v92 : bool = pyteal.Assert(v91)
+    v93 : Any = TxnGroup.__getitem__(Gtxn, 2)
+    v96 : bool = (v93.receiver()) == (pyteal.App.globalGet(v5))
+    v97 : bool = pyteal.Assert(v96)
+    v98 : Any = TxnGroup.__getitem__(Gtxn, 2)
+    v101 : bool = (v98.amount()) == (pyteal.App.globalGet(v2_1))
+    v103 : List[bool] = [v77, v82, v87, v92, v97, pyteal.Assert(v101)]
+    v104 : bool = pyteal.Seq(v103)
+    v105 : Any = TxnGroup.__getitem__(Gtxn, 1)
+    v108 : bool = (v105.type_enum()) == pyteal.TxnType.Payment
+    v109 : bool = pyteal.Assert(v108)
+    v110 : Any = TxnGroup.__getitem__(Gtxn, 1)
+    v113 : bool = (v110.sender()) == (pyteal.App.globalGet(v3_1))
+    v114 : bool = pyteal.Assert(v113)
+    v115 : List[bool] = [v23, v53]
+    v116 : List[bool] = [v37, v69]
+    v117 : List[bool] = [v72, v104]
+    v118 : bool = pyteal.Cond(v115, v116, v117)
+    v119 : int = (pyteal.Int(1)) or 0
+    v121 : List[bool] = [v109, v114, v118, pyteal.Return(v119)]
+    return pyteal.Seq(v121)
 
 
 v0 : str = "main.spi Loaded"
 
 to_console(interpolate("%A%P()", [v0]))
 
-def arrow_0(__unit: Any=None) -> Any:
+def arrow_0(__unit: Any=None) -> bool:
     return closure0(None, None)
 
 
-v1 : Callable[[], Any] = arrow_0
+v1 : Callable[[], bool] = arrow_0
 
 def arrow_1(__unit: Any=None) -> bool:
     return closure1(None, None)
@@ -79,9 +155,15 @@ def arrow_1(__unit: Any=None) -> bool:
 
 v2 : Callable[[], bool] = arrow_1
 
-def arrow_2(v0_1: int) -> Any:
+def arrow_2(v0_1: int) -> bool:
     return closure2(None, v0_1)
 
 
-v3 : Callable[[int], Any] = arrow_2
+v3 : Callable[[int], bool] = arrow_2
+
+def arrow_3(__unit: Any=None) -> bool:
+    return closure3(None, None)
+
+
+v4 : Callable[[], bool] = arrow_3
 
