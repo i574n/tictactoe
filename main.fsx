21375f3a264355451c519063565aa6d5b20c999b
fc1943s <root@fc1943s.dev>
2022-05-19 02:52:34 +0000

    more fns

--- main.fsx
+++ main.fsx
@@ -1,59 +1,85 @@
+let rec method0 () : struct (string * string * string * string * string * string * string * string * string) =
+    let v0 : string = (importAll "pyteal")?Bytes "PlayerXState"
+    let v1 : string = (importAll "pyteal")?Bytes "PlayerOState"
+    let v2 : string = (importAll "pyteal")?Bytes "PlayerXAddress"
+    let v3 : string = (importAll "pyteal")?Bytes "PlayerOAddress"
+    let v4 : string = (importAll "pyteal")?Bytes "PlayerTurnAddress"
+    let v5 : string = (importAll "pyteal")?Bytes "FundsEscrowAddress"
+    let v6 : string = (importAll "pyteal")?Bytes "ActionTimeout"
+    let v7 : string = (importAll "pyteal")?Bytes "GameState"
+    let v8 : string = (importAll "pyteal")?Bytes "BetAmount"
+    struct (v6, v8, v5, v7, v3, v1, v4, v2, v0)
+and closure0 () () : obj =
+    let v0 : string = "app_initialization_logic"
+    printfn $"%A{v0}"
+    let v1 : int32 = (importAll "pyteal")?Int 0
+    let v2 : int32 = (importAll "pyteal")?Int 0
+    let v3 : int32 = (importAll "pyteal")?Int 0
+    let v4 : int32 = (importAll "pyteal")?Int 1000000
+    let v5 : int32 = (importAll "pyteal")?Int 3600
+    let struct (v6 : string, v7 : string, v8 : string, v9 : string, v10 : string, v11 : string, v12 : string, v13 : string, v14 : string) = method0()
+    let v15 : obj = (importAll "pyteal")?App?globalPut (v14, v1)
+    let v16 : obj = (importAll "pyteal")?App?globalPut (v11, v2)
+    let v17 : obj = (importAll "pyteal")?App?globalPut (v9, v3)
+    let v18 : obj = (importAll "pyteal")?App?globalPut (v7, v4)
+    let v19 : int32 = (importAll "pyteal")?Int 1
+    let v20 : obj = (importAll "pyteal")?Return v19
+    let v21 : (obj []) = [|v15; v16; v17; v18; v20|]
+    (importAll "pyteal")?Seq v21
+and closure1 () () : bool =
+    let v0 : string = "is_tie"
+    printfn $"%A{v0}"
+    let struct (v1 : string, v2 : string, v3 : string, v4 : string, v5 : string, v6 : string, v7 : string, v8 : string, v9 : string) = method0()
+    let v10 : int32 = (importAll "pyteal")?App?globalGet v9
+    let v11 : int32 = (importAll "pyteal")?App?globalGet v6
+    let v12 : int32 = (importAll "pyteal")?Int 511
+    let v13 : int32 = (importAll "pyteal")?BitwiseOr v10 v11
+    v12 = v13
+and closure2 () (v0 : int32) : obj =
+    let v1 : string = "has_player_won"
+    printfn $"%A{v1}"
+    let v2 : int32 = (importAll "pyteal")?Int 448
+    let v3 : int32 = (importAll "pyteal")?BitwiseAnd v0 v2
+    let v4 : int32 = (importAll "pyteal")?Int 448
+    let v5 : bool = v3 = v4
+    let v6 : int32 = (importAll "pyteal")?Int 56
+    let v7 : int32 = (importAll "pyteal")?BitwiseAnd v0 v6
+    let v8 : int32 = (importAll "pyteal")?Int 56
+    let v9 : bool = v7 = v8
+    let v10 : int32 = (importAll "pyteal")?Int 7
+    let v11 : int32 = (importAll "pyteal")?BitwiseAnd v0 v10
+    let v12 : int32 = (importAll "pyteal")?Int 7
+    let v13 : bool = v11 = v12
+    let v14 : int32 = (importAll "pyteal")?Int 292
+    let v15 : int32 = (importAll "pyteal")?BitwiseAnd v0 v14
+    let v16 : int32 = (importAll "pyteal")?Int 292
+    let v17 : bool = v15 = v16
+    let v18 : int32 = (importAll "pyteal")?Int 146
+    let v19 : int32 = (importAll "pyteal")?BitwiseAnd v0 v18
+    let v20 : int32 = (importAll "pyteal")?Int 146
+    let v21 : bool = v19 = v20
+    let v22 : int32 = (importAll "pyteal")?Int 73
+    let v23 : int32 = (importAll "pyteal")?BitwiseAnd v0 v22
+    let v24 : int32 = (importAll "pyteal")?Int 73
+    let v25 : bool = v23 = v24
+    let v26 : int32 = (importAll "pyteal")?Int 273
+    let v27 : int32 = (importAll "pyteal")?BitwiseAnd v0 v26
+    let v28 : int32 = (importAll "pyteal")?Int 273
+    let v29 : bool = v27 = v28
+    let v30 : int32 = (importAll "pyteal")?Int 84
+    let v31 : int32 = (importAll "pyteal")?BitwiseAnd v0 v30
+    let v32 : int32 = (importAll "pyteal")?Int 84
+    let v33 : bool = v31 = v32
+    let v34 : obj = (importAll "pyteal")?Or v5 v9 v13 v17 v21 v25 v29 v33
+    let v35 : int32 = (importAll "pyteal")?Int 1
+    let v36 : int32 = (importAll "pyteal")?Int 0
+    (importAll "pyteal")?If v34 v35 v36
 #r "nuget: Fable.Python"
 open Fable.Core.PyInterop
 open Fable.Core
-let rec method0 (v0 : int32) : int32 =
-    let v1 : (int32 -> int32) = import "Int" "pyteal"
-    v1 v0
-and method1 (v0 : string) : string =
-    let v1 : (string -> string) = import "Bytes" "pyteal"
-    v1 v0
-and method2 (v0 : string, v1 : int32) : obj =
-    (import "App" "pyteal")?globalPut(v0, v1)
-and method3 (v0 : int32) : obj =
-    let v1 : (int32 -> obj) = import "Return" "pyteal"
-    v1 v0
-and method4 (v0 : (obj [])) : obj =
-    let v1 : ((obj []) -> obj) = import "Seq" "pyteal"
-    v1 v0
-and closure0 () () : obj =
-    let v0 : int32 = 0
-    let v1 : int32 = method0(v0)
-    let v2 : int32 = 0
-    let v3 : int32 = method0(v2)
-    let v4 : int32 = 0
-    let v5 : int32 = method0(v4)
-    let v6 : int32 = 1000000
-    let v7 : int32 = method0(v6)
-    let v8 : int32 = 3600
-    let v9 : int32 = method0(v8)
-    let v10 : string = "PlayerXState"
-    let v11 : string = method1(v10)
-    let v12 : string = "PlayerOState"
-    let v13 : string = method1(v12)
-    let v14 : string = "PlayerXAddress"
-    let v15 : string = method1(v14)
-    let v16 : string = "PlayerOAddress"
-    let v17 : string = method1(v16)
-    let v18 : string = "PlayerTurnAddress"
-    let v19 : string = method1(v18)
-    let v20 : string = "FundsEscrowAddress"
-    let v21 : string = method1(v20)
-    let v22 : string = "ActionTimeout"
-    let v23 : string = method1(v22)
-    let v24 : string = "GameState"
-    let v25 : string = method1(v24)
-    let v26 : string = "BetAmount"
-    let v27 : string = method1(v26)
-    let v28 : obj = method2(v11, v1)
-    let v29 : obj = method2(v13, v3)
-    let v30 : obj = method2(v25, v5)
-    let v31 : obj = method2(v27, v7)
-    let v32 : int32 = 1
-    let v33 : int32 = method0(v32)
-    let v34 : obj = method3(v33)
-    let v35 : (obj []) = [|v28; v29; v30; v31; v34|]
-    method4(v35)
 let v0 : string = "main.spi Loaded"
 printfn $"%A{v0}"
 let v1 : (unit -> obj) = closure0()
+let v2 : (unit -> bool) = closure1()
+let v3 : (int32 -> obj) = closure2()
 ()
--- main.py
+++ main.py
@@ -1,47 +1,66 @@
-from pyteal import (Int, Bytes, App, Return, Seq)
-from typing import (Callable, Any, List)
+import pyteal
+from typing import (Tuple, Any, List, Callable)
 from fable_modules.fable_library.string import (to_console, interpolate)
 
-def method0(v0_1: int) -> int:
-    v1_1 : Callable[[int], int] = Int
-    return v1_1(v0_1)
-
-
-def method1(v0_1: str) -> str:
-    v1_1 : Callable[[str], str] = Bytes
-    return v1_1(v0_1)
-
-
-def method2(v0_1: str, v1_1: int) -> Any:
-    return App.globalPut(v0_1, v1_1)
-
-
-def method3(v0_1: int) -> Any:
-    v1_1 : Callable[[int], Any] = Return
-    return v1_1(v0_1)
-
-
-def method4(v0_1: List[Any]) -> Any:
-    v1_1 : Callable[[List[Any]], Any] = Seq
-    return v1_1(v0_1)
+def method0() -> Tuple[str, str, str, str, str, str, str, str, str]:
+    v0_1 : str = pyteal.Bytes("PlayerXState")
+    v1_1 : str = pyteal.Bytes("PlayerOState")
+    v2_1 : str = pyteal.Bytes("PlayerXAddress")
+    v3_1 : str = pyteal.Bytes("PlayerOAddress")
+    v4 : str = pyteal.Bytes("PlayerTurnAddress")
+    v5 : str = pyteal.Bytes("FundsEscrowAddress")
+    v6 : str = pyteal.Bytes("ActionTimeout")
+    v7 : str = pyteal.Bytes("GameState")
+    return (v6, pyteal.Bytes("BetAmount"), v5, v7, v3_1, v1_1, v4, v2_1, v0_1)
 
 
 def closure0(unit_var0: None, unit_var1: None) -> Any:
-    v1_1 : int = method0(0) or 0
-    v3 : int = method0(0) or 0
-    v5 : int = method0(0) or 0
-    v7 : int = method0(1000000) or 0
-    v9 : int = method0(3600) or 0
-    v11 : str = method1("PlayerXState")
-    v13 : str = method1("PlayerOState")
-    v15 : str = method1("PlayerXAddress")
-    v17 : str = method1("PlayerOAddress")
-    v19 : str = method1("PlayerTurnAddress")
-    v21 : str = method1("FundsEscrowAddress")
-    v23 : str = method1("ActionTimeout")
-    v25 : str = method1("GameState")
-    v27 : str = method1("BetAmount")
-    return method4([method2(v11, v1_1), method2(v13, v3), method2(v25, v5), method2(v27, v7), method3(method0(1))])
+    to_console(interpolate("%A%P()", ["app_initialization_logic"]))
+    v1_1 : int = (pyteal.Int(0)) or 0
+    v2_1 : int = (pyteal.Int(0)) or 0
+    v3_1 : int = (pyteal.Int(0)) or 0
+    v4 : int = (pyteal.Int(1000000)) or 0
+    v5 : int = (pyteal.Int(3600)) or 0
+    pattern_input : Tuple[str, str, str, str, str, str, str, str, str] = method0()
+    v15 : Any = pyteal.App.globalPut(pattern_input[8], v1_1)
+    v16 : Any = pyteal.App.globalPut(pattern_input[5], v2_1)
+    v17 : Any = pyteal.App.globalPut(pattern_input[3], v3_1)
+    v18 : Any = pyteal.App.globalPut(pattern_input[1], v4)
+    v19 : int = (pyteal.Int(1)) or 0
+    v21 : List[Any] = [v15, v16, v17, v18, pyteal.Return(v19)]
+    return pyteal.Seq(v21)
+
+
+def closure1(unit_var0: None, unit_var1: None) -> bool:
+    to_console(interpolate("%A%P()", ["is_tie"]))
+    pattern_input : Tuple[str, str, str, str, str, str, str, str, str] = method0()
+    v10 : int = (pyteal.App.globalGet(pattern_input[8])) or 0
+    v11 : int = (pyteal.App.globalGet(pattern_input[5])) or 0
+    return (pyteal.Int(511)) == (pyteal.BitwiseOr(v10, v11))
+
+
+def closure2(unit_var0: None, v0_1: int) -> Any:
+    to_console(interpolate("%A%P()", ["has_player_won"]))
+    v2_1 : int = (pyteal.Int(448)) or 0
+    v5 : bool = (pyteal.BitwiseAnd(v0_1, v2_1)) == (pyteal.Int(448))
+    v6 : int = (pyteal.Int(56)) or 0
+    v9 : bool = (pyteal.BitwiseAnd(v0_1, v6)) == (pyteal.Int(56))
+    v10 : int = (pyteal.Int(7)) or 0
+    v13 : bool = (pyteal.BitwiseAnd(v0_1, v10)) == (pyteal.Int(7))
+    v14 : int = (pyteal.Int(292)) or 0
+    v17 : bool = (pyteal.BitwiseAnd(v0_1, v14)) == (pyteal.Int(292))
+    v18 : int = (pyteal.Int(146)) or 0
+    v21 : bool = (pyteal.BitwiseAnd(v0_1, v18)) == (pyteal.Int(146))
+    v22 : int = (pyteal.Int(73)) or 0
+    v25 : bool = (pyteal.BitwiseAnd(v0_1, v22)) == (pyteal.Int(73))
+    v26 : int = (pyteal.Int(273)) or 0
+    v29 : bool = (pyteal.BitwiseAnd(v0_1, v26)) == (pyteal.Int(273))
+    v30 : int = (pyteal.Int(84)) or 0
+    v33 : bool = (pyteal.BitwiseAnd(v0_1, v30)) == (pyteal.Int(84))
+    v34 : Any = pyteal.Or(v5, v9, v13, v17, v21, v25, v29, v33)
+    v35 : int = (pyteal.Int(1)) or 0
+    v36 : int = (pyteal.Int(0)) or 0
+    return pyteal.If(v34, v35, v36)
 
 
 v0 : str = "main.spi Loaded"
@@ -53,4 +72,16 @@ def arrow_0(__unit: Any=None) -> Any:
 
 
 v1 : Callable[[], Any] = arrow_0
+
+def arrow_1(__unit: Any=None) -> bool:
+    return closure1(None, None)
+
+
+v2 : Callable[[], bool] = arrow_1
+
+def arrow_2(v0_1: int) -> Any:
+    return closure2(None, v0_1)
+
+
+v3 : Callable[[int], Any] = arrow_2
 
