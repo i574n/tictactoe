555c2fa18765a0d6f33d6128b3c69668f04a4360
fc1943s <root@fc1943s.dev>
2022-06-01 22:58:23 +0000

    nbdev + node

---
+++ spiral.ts
@@ -0,0 +1,38 @@
+import * as zmq from "zeromq"
+import * as fs from "fs"
+import * as util from "./util"
+
+
+// cell
+var requestRun = async (prev: Promise<string | null>, file: any): Promise<string | null> => {
+    const port: number = 13805
+    await prev // Waiting on the previous request is so they get ordered properly. Otherwise, messages might fill up and fire in arbitrary order.
+    const sock = new zmq.Request()
+    const uriServer = `tcp://localhost:${port}`
+    sock.connect(uriServer)
+    await sock.send(JSON.stringify(file))
+    const [x] = await sock.receive()
+    sock.disconnect(uriServer)
+    return x ? x.toString() : null
+}
+var prev_request: Promise<string | null> = new Promise(resolve => resolve(null))
+var request = async (file: any) => { prev_request = requestRun(prev_request, file); return prev_request }
+var requestJSON = (file: any) => request(file).then(x => x ? JSON.parse(x) : undefined)
+
+var spiBuildFileReq = async (uri: string, backend: string): Promise<void> => requestJSON({ BuildFile: { uri, backend } })
+
+// cell
+export var spiToFsx = async (spiPath: string) => {
+    const fsxPath = spiPath.replace('.spi', '.fsx')
+    await spiBuildFileReq(spiPath, 'Fsharp')
+    await util.waitFileChange(fsxPath)
+    const lines = fs.readFileSync(fsxPath).toString().split('\n')
+    const [imports, code] = lines.reduce(
+        ([imports, code], line) =>
+            /^(open|\#r) .*?$/.test(line)
+                ? [[...imports, line], code]
+                : [imports, [...code, line]]
+        , [[], []]
+    );
+    fs.writeFileSync(fsxPath, [...imports, '', ...code].join('\n'))
+}
---
+++ util.ts
@@ -0,0 +1,21 @@
+import * as fs from "fs"
+
+
+// cell
+var start = process.hrtime()
+
+export var elapsed = (start: [number, number]) => process.hrtime(start)[1] / 1000000
+export var logStep = (note: string) => {
+    console.log(`${process.hrtime(start)[0]} s, ${elapsed(start).toFixed(3)} ms - ${note}`)
+    start = process.hrtime()
+}
+export var sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+export var waitFileChange = async (path: string) => {
+    start = process.hrtime()
+    const stat_mtime = fs.statSync(path).mtimeMs
+    while (elapsed(start) < 30000
+        && (fs.statSync(path).mtimeMs === stat_mtime
+            || fs.readFileSync(path).length === 0)) {
+        await sleep(50)
+    }
+}
