c6795bdbaa0199859a8a1dbb212b33dc1e50dcb5
fc1943s <root@fc1943s.dev>
2022-08-11 16:50:37 +0000

    ci

--- web/playwright.config.ts
+++ web/playwright.config.ts
@@ -24,9 +24,9 @@ const config: PlaywrightTestConfig = {
         { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
         ...(!process.env.CI ? [] : [
             { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
-            { name: 'webkit', use: { ...devices['Desktop Safari'] } },
+            // { name: 'webkit', use: { ...devices['Desktop Safari'] } },
             { name: 'Mobile Chrome', use: { ...devices['Pixel 5'] } },
-            { name: 'Mobile Safari', use: { ...devices['iPhone 12'] } },
+            // { name: 'Mobile Safari', use: { ...devices['iPhone 12'] } },
             { name: 'Microsoft Edge', use: { channel: 'msedge' } },
             { name: 'Google Chrome', use: { channel: 'chrome' } }
         ])
