9f72af7e7d0f9bd940aaa25c11f2468a967626ca
fc1943s <root@fc1943s.dev>
2022-12-20 22:02:58 +0000

    tests

--- lib_spi/supervisor/src/main.rs
+++ lib_spi/supervisor/src/main.rs
@@ -14,14 +14,6 @@ struct Args {
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Args = <Args as clap::Parser>::parse();
     let spi_to_fsx = supervisor::Supervisor::closure1;
-    // let fsx = spi_to_fsx(
-    //     std::rc::Rc::new(fable_library_rust::Native_::MutCell::new(Box::from(
-    //         args.spi_path.to_path().strip_prefix("").unwrap(),
-    //     ))),
-    //     Some(std::rc::Rc::new(fable_library_rust::Native_::MutCell::new(
-    //         Box::from(args.fsx_path.to_path().strip_prefix("").unwrap()),
-    //     ))),
-    // );
     let fsx = spi_to_fsx(
         std::path::PathBuf::from(&args.spi_path),
         Some(std::path::PathBuf::from(&args.fsx_path)),
