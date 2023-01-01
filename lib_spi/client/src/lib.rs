ca01f1033c956183647fd0bc00097037c4b7b236
fc1943s <root@fc1943s.dev>
2023-01-01 17:33:55 -0300

    tests

--- lib_spi/client/src/lib.rs
+++ lib_spi/client/src/lib.rs
@@ -5,7 +5,7 @@ pub mod client;
 #[wasm_bindgen]
 pub fn client() -> i32 {
     web_sys::console::log_1(&"Hello using web-sys".into());
-    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+    // std::panic::set_hook(Box::new(console_error_panic_hook::hook));
     let a = client::Client::v0()();
     println!("a={}", a);
     2i32
